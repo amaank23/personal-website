18667edadcf3429a7967c555a7b6b39f16e130cd
unknown <amaanwaseem24@gmail.com>
2024-06-09 15:53:34 +0500

    added dark and light theme in navlinks

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -1,10 +1,17 @@
 import { PropTypes } from "prop-types";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/themeContext";
+import { themeObject } from "../../utils/utils";
 
 const NavLink = ({ label, onClickNavigate }) => {
+  const themeContext = useContext(ThemeContext);
   return (
     <span
-      className="text-lg cursor-pointer text-white"
+      className="text-lg cursor-pointer"
       onClick={onClickNavigate}
+      style={{
+        color: themeObject[themeContext.theme].textColor,
+      }}
     >
       {label}
     </span>
